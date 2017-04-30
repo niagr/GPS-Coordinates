90300f16b0a16fcff4b6bb0c976286ed0e0c594c
Nishant George Agrwal <nishant.george@hashedin.com>
2017-04-30 18:55:06 +0530

    Changed display endpoint from GET to POST along with other changes:
    - removed stray print statements
    - added print statement to log location updates with trip ID

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const display = require('./display');
 const current_coordinates = require(__dirname +'/getCoordinates/current_coordinates');
 
 app.use('/',(req,res,next) => {
-    console.log('Time is ' + Date.now());
-    console.log(req.method);
     //When the middleware doesn't send back the response object, it must pass control using 'next()' 
     next();
 });
