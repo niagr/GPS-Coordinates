89707f2793bd7d2535e7009a1aa9cb737f1e60bb
Maverick96 <harshpancholi8@gmail.com>
2017-05-01 00:37:51 +0530

    Find coordinates using RouteID

--- getCoordinates/current_coordinates.js
+++ getCoordinates/current_coordinates.js
@@ -6,8 +6,8 @@ const search = require('./id_query');
 router.use(bodyParser.urlencoded({extended : true}));
 
 router.get('/',(req,res)=> {
-    const id = req.query['trip_id']; 
-    search(req,res,id);
+    const route_id = req.query['route_id']; 
+    search(req,res,route_id);
 });
 
 module.exports = router;
--- getCoordinates/id_query.js
+++ getCoordinates/id_query.js
@@ -1,24 +1,30 @@
 const pool = require('../pg-pool');
 
-function search(req,res,id){
+function search(req,res,route_id){
     pool.connect((err,client,release) => {
         if(err){
             res.send('Connection Failed')
             return console.log("Error" + err);
         }
         //to store the row returned from query
-         var results = [];
+         var trip_id = [];
+         console.log(typeof route_id);
          //query to get current coordinates
-       client.query("SELECT latitude,longitude FROM location WHERE trip_id='" + id +"' ORDER BY sr_no DESC LIMIT 1",
-         (err, results) => {
-            //return the client back after you are done using it 
-            release();
+       client.query("SELECT trip_id FROM gettripid WHERE route_id =" +route_id ,
+         (err, trip_id) => {
              if(err){
                  return console.log(err);
             }
-            
-            //'results.rows' contains the data of the rows selected from the query
-            res.send(results.rows);
+            console.log(trip_id.rows[0].trip_id);
+            client.query("SELECT latitude,longitude FROM location WHERE trip_id='" + trip_id.rows[0].trip_id + "' ORDER BY sr_no DESC LIMIT 1",
+            (err,coordinates) =>{
+                if(err)
+                 return console.log(err);       
+                
+                console.log(coordinates);
+                res.send(coordinates.rows); 
+           });
+        // res.end();                    
          });
     });
 }
